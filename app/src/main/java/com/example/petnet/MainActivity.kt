6fbdc77e1319e3b5a9fa62386eb6cced40d83e0c
BartuRhymlhold <bartuturkkan@gmail.com>
2024-12-10 15:55:48 +0300

    ui.theme updated for project theme
    ui.theme: Color.kt, Theme.kt, Type.kt has been updated

--- app/src/main/java/com/example/petnet/MainActivity.kt
+++ app/src/main/java/com/example/petnet/MainActivity.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.petnet.ui.theme.PetnetTheme
+import com.example.compose.PetnetTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
